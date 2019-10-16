e22f6dbfc9bf0594fcf97c040b04d626d464754d
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-16 16:47:56 -0600

    done

--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -20,6 +20,12 @@ export default class Edit extends Component {
   }
 
   updatePost() {
+    const {text} = this.state
+    const {id, updatePostFn, hideEdit} = this.props
+        console.log(id, text)
+    updatePostFn(id, text);
+    hideEdit();
+
 
   }
 
